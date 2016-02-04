c58cee8217557cec20113e465cb6f0d1d22983aa
DongDongJu <commisori28@gmail.com>
2016-02-04 04:23:38 +0000

    gpio set

--- spi.c
+++ spi.c
@@ -1,6 +1,9 @@
 #include <bcm2835.h>
 #include <stdio.h>
 
+#define CONVST RPI_V2_GPIO_P1_29
+#define BUSY RPI_V2_GPIO_P1_31
+#define RESET RPI_V2_GPIO_P1_33
 int main(int argc,char **argv)
 {
 	char data_buffer[8];
@@ -10,6 +13,14 @@ int main(int argc,char **argv)
 		printf("init failed!");
 		return 1;
 	}
+	//--------------config gpio
+	bcm2835_gpio_fsel(CONVST,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(BUSY,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(RESET,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_write(CONVST,LOW);
+	bcm2835_gpio_write(BUSY,LOW);
+	bcm2835_gpio_write(RESET,LOW);
+	//------------------------
 	bcm2835_spi_begin();
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
 	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
@@ -18,14 +29,18 @@ int main(int argc,char **argv)
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0,LOW);
 	while(1)
 	{
+		//-------------------start
+		bcm2835_gpio_write(CONVST,high);
+		bcm2835_gpio_write(BUSY,high);
 		for(i=0;i<8;i++)
 			data_buffer[i] = 0x00 + i;
 		bcm2835_spi_transfern(&data_buffer[0],8);
 		for(i=0;i<8;i++)
 			printf("0x%02X\n",data_buffer[i]);
-		bcm2835_spi_end();
-		bcm2835_close();
-		bcm2835_delayMicroseconds(1000);
+		bcm2835_delayMicroseconds(300);
+		bcm2835_gpio_write(CONVST,low);
+		bcm2835_gpio_write(BUSY,low);
 	}
+	bcm2835_close();
 	return 0;
 }
